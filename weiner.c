803848f56d807da7758277c1708f565faf46ef53
Bruno Reis <brunohenriquereisalmeida@gmail.com>
2026-01-19 15:22:29 +0100

    add explaining comments

--- weiner.c
+++ weiner.c
@@ -9,13 +9,19 @@ typedef char boolean;
 
 typedef struct Node;
 
+// Suffix-tree node struct
 typedef struct Node{
+    // matrix which represents the label of the edge, in which the line is the next symbol, the 1st column is the beggining and the 2nd is the ending index 
     int limits[256][2];
+    // the children pointers for each of the possible symbols
     Node** child[256];
+    // the a-link for each of the possible symbols
     Node** a_link[256];  
+    // the a-test for each of the possible symbols
     boolean a_test[256];  
 } Node;
 
+// Suffix-tree struct
 typedef struct Suf_tree{
     Node* root;
     Node* cur_leaf;
@@ -27,6 +33,7 @@ Node* breaknode(Node* w1, Node* w2, Node* v1, Node* v2){
     }
 }
 
+// stack struct for storing the last path traversed, for the step 'Up' from 'Up-Link-Down'
 typedef struct Stack{
     Node** el;
     int capacity, size;
